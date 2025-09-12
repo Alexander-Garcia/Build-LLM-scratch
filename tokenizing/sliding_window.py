02b1200dbc92e92b6cc78d09be9021dcf8b8a5af
Alexander <31865557+Alexander-Garcia@users.noreply.github.com>
2025-09-12 16:03:56 -0500

    feat(tokenizing): embeddings

--- tokenizing/sliding_window.py
+++ tokenizing/sliding_window.py
@@ -112,3 +112,47 @@ data_iter = iter(dataloader)
 inputs, targets = next(data_iter)
 print("Inputs: ", inputs)
 print("Targets: ", targets)
+
+
+# create some dummy input IDs
+input_ids = torch.tensor([2, 3, 5, 1])
+vocab_size = 6
+output_dim = 3
+torch.manual_seed(123)
+# instantiate embedding layer
+# will be 6 rows and 3 columns
+# 1 row for each of the six possible tokens in vocab and 1 column for each of the 3 embedding dimensions
+embedding_layer = torch.nn.Embedding(vocab_size, output_dim)
+print(embedding_layer.weight)
+
+print(embedding_layer(torch.tensor([3])))
+
+
+# apply embedding to all 4 input IDs
+print(embedding_layer(input_ids))
+
+
+vocab_size = 50257
+output_dim = 256
+token_embedding_layer = torch.nn.Embedding(vocab_size, output_dim)
+max_length = 4
+dataloader = create_dataloader_v1(
+    raw_text, batch_size=8, max_length=max_length, stride=max_length, shuffle=False
+)
+data_iter = iter(dataloader)
+inputs, targets = next(data_iter)
+# token ID tensor is 8x4 dim = 8 samples with 4 tokens each
+print("Token IDs:\n", inputs)
+print("\nInputs shape:\n", inputs.shape)
+
+token_embeddings = token_embedding_layer(inputs)
+print(token_embeddings.shape)
+
+
+context_length = max_length
+pos_embedding_layer = torch.nn.Embedding(context_length, output_dim)
+pos_embeddings = pos_embedding_layer(torch.arange(context_length))
+print(pos_embeddings.shape)
+
+input_embeddings = token_embeddings + pos_embeddings
+print(input_embeddings.shape)
